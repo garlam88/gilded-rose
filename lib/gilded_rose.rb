ab61c3efab4249695b1cc157c659c9400d997ed5
Garry <Garry@Garrys-iMac.local>
2019-03-09 18:31:42 -0500

    More refactoring

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,7 +4,7 @@ class GildedRose
   AGED_BRIE = "Aged Brie"
   BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
   SULFURAS = "Sulfuras, Hand of Ragnaros"
-  CONJURED = "Conjured Mana Cake"
+  CONJURED_MANA = "Conjured Mana Cake"
   NORMAL_ITEM = "Normal Item"
   MODERATE_QUALITY = 10
   MAX_QUALITY = 50
@@ -29,60 +29,65 @@ class GildedRose
   end
 
   def update_quality
-    adjustment = 0
-    if @name == AGED_BRIE
-      if @days_remaining == ON_SELL_DAYS
-        if @quality != MAX_QUALITY
-          if @quality == MODERATE_QUALITY
-           adjustment = 2
-         else
-            adjustment = 1
-         end
-       end
-
-      else
-        if @days_remaining == BEFORE_SELL_DAYS
-          if @quality == MODERATE_QUALITY
-            adjustment = 1
-          end
-        else
-          if @days_remaining == AFTER_SELL_DAYS
-            adjustment = 2 unless @quality == MAX_QUALITY
-          end
-        end
+    adjustment = (case @name
+      when AGED_BRIE then calculate_aged_brie_adjustment 
+      when BACKSTAGE_PASSES then calculate_backstage_pass_adjustment 
+      when CONJURED_MANA then calculate_conjured_mana_adjustment 
+      when NORMAL_ITEM then calculate_normal_item_adjustment
+      else 0
+    end)
+    @quality += adjustment
+  end
+  
+  def calculate_aged_brie_adjustment 
+    val = 0
+    if @days_remaining == ON_SELL_DAYS
+      if @quality != MAX_QUALITY
+        val = @quality == MODERATE_QUALITY ? 2 : 1
       end
     else
-      if @name == BACKSTAGE_PASSES
-        if @quality != MAX_QUALITY
-          case @days_remaining
-            when -10,0 then adjustment = -10
-            when 1, 5 then adjustment = 3
-            when 6, 10 then adjustment = 2 
-            when 11 then adjustment = 1 
-          end
-
-        end   
+      if @days_remaining == BEFORE_SELL_DAYS
+        if @quality == MODERATE_QUALITY
+          val = 1
+        end
       else
-        if @name == CONJURED
-          if @quality != NO_QUALITY
-            case @days_remaining
-              when -10, 0 then adjustment = -4
-              when 5 then adjustment = -2
-            end 
-          end
-        else
-          if @name == NORMAL_ITEM
-            if @quality > NO_QUALITY
-              if @days_remaining > ON_SELL_DAYS
-                adjustment = -1
-              else
-                adjustment = -2
-              end
-            end
-          end
+        if @days_remaining == AFTER_SELL_DAYS
+         val = 2 unless @quality == MAX_QUALITY
         end
       end
     end
-    @quality += adjustment
+    return val
   end
+
+   def calculate_backstage_pass_adjustment 
+     val = 0
+     if @quality != MAX_QUALITY
+       case @days_remaining
+         when -10,0 then val = -10
+         when 1, 5 then val = 3
+         when 6, 10 then val = 2 
+         when 11 then val = 1 
+       end
+     end
+     return val
+   end
+
+  def calculate_conjured_mana_adjustment
+    val = 0
+    if @quality != NO_QUALITY
+      case @days_remaining
+        when -10, 0 then val = -4
+        when 5 then val = -2
+      end 
+    end
+    return val
+  end
+
+  def calculate_normal_item_adjustment 
+    if @quality > NO_QUALITY
+      return @days_remaining > ON_SELL_DAYS ? -1 : -2
+    end
+    return 0
+  end
+
 end
