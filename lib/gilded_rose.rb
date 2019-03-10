9e5cdfdd8303205abccea807d1ded07bfa524e8a
Garry <Garry@Garrys-iMac.local>
2019-03-10 11:03:31 -0400

    Cleaned up formatting and order of constants

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,15 +2,17 @@ class GildedRose
   attr_reader :name, :days_remaining, :quality
   
   AGED_BRIE = "Aged Brie"
-  BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
-  SULFURAS = "Sulfuras, Hand of Ragnaros"
-  CONJURED_MANA = "Conjured Mana Cake"
   NORMAL_ITEM = "Normal Item"
-  MODERATE_QUALITY = 10
-  MAX_QUALITY = 50
+  CONJURED_MANA = "Conjured Mana Cake"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+  BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+  
   NO_QUALITY = 0
-  BEFORE_SELL_DAYS = 5
+  MAX_QUALITY = 50
+  MODERATE_QUALITY = 10
+
   ON_SELL_DAYS = 0
+  BEFORE_SELL_DAYS = 5
   AFTER_SELL_DAYS = -10
 
   def initialize(name:, days_remaining:, quality:)
@@ -46,21 +48,23 @@ class GildedRose
     return val.nil? ? 0 : val
   end
 
-   def calculate_backstage_pass_adjustment 
-     case @days_remaining
-        when -10,0 then val = -10
-        when 1, 5 then val = 3
-        when 6, 10 then val = 2 
-        when 11 then val = 1 
+  def calculate_backstage_pass_adjustment 
+    val =
+      case @days_remaining
+        when -10, 0 then -10
+        when 1, 5 then 3
+        when 6, 10 then 2 
+        when 11 then 1 
       end unless @quality == MAX_QUALITY
-     return val.nil? ? 0 : val
-   end
+    return val.nil? ? 0 : val
+  end
 
   def calculate_conjured_mana_adjustment
-    case @days_remaining
-      when -10, 0 then val = -4
-      when 5 then val = -2
-    end unless @quality == NO_QUALITY
+    val = 
+      case @days_remaining
+        when -10, 0 then -4
+        when 5 then -2
+      end unless @quality == NO_QUALITY
     return val.nil? ? 0 : val
   end
 
